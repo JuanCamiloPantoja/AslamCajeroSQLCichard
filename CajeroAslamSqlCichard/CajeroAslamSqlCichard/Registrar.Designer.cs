178bb67a9cd75bd981f268d8da0b2468686efe58
Ash20 <Ash20@CAMILO>
2024-08-27 00:21:15 -0500

    Commit registrar

--- CajeroAslamSqlCichard/CajeroAslamSqlCichard/Registrar.Designer.cs
+++ CajeroAslamSqlCichard/CajeroAslamSqlCichard/Registrar.Designer.cs
@@ -37,24 +37,27 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.NumDoc = new System.Windows.Forms.TextBox();
             this.btGuardarRegis = new System.Windows.Forms.Button();
             this.BtCancelar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(121, 64);
+            this.label1.Location = new System.Drawing.Point(122, 41);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(56, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(121, 122);
+            this.label2.Location = new System.Drawing.Point(122, 95);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 16);
             this.label2.TabIndex = 1;
@@ -63,7 +66,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(121, 186);
+            this.label3.Location = new System.Drawing.Point(122, 152);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(40, 16);
             this.label3.TabIndex = 2;
@@ -72,17 +75,16 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(121, 312);
+            this.label4.Location = new System.Drawing.Point(122, 260);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(146, 16);
             this.label4.TabIndex = 3;
             this.label4.Text = "Número de Documento";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(121, 249);
+            this.label5.Location = new System.Drawing.Point(121, 206);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(126, 16);
             this.label5.TabIndex = 4;
@@ -96,38 +98,39 @@
             "C.C",
             "P.P",
             "C.E"});
-            this.comboBox1.Location = new System.Drawing.Point(340, 246);
+            this.comboBox1.Location = new System.Drawing.Point(340, 206);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(154, 24);
             this.comboBox1.TabIndex = 5;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(340, 58);
+            this.textBox1.Location = new System.Drawing.Point(340, 35);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(254, 22);
             this.textBox1.TabIndex = 6;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(340, 119);
+            this.textBox2.Location = new System.Drawing.Point(340, 95);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(254, 22);
             this.textBox2.TabIndex = 7;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(340, 180);
+            this.textBox3.Location = new System.Drawing.Point(340, 152);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(254, 22);
             this.textBox3.TabIndex = 8;
             // 
-            // textBox4
+            // NumDoc
             // 
-            this.textBox4.Location = new System.Drawing.Point(340, 306);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(254, 22);
-            this.textBox4.TabIndex = 9;
+            this.NumDoc.Location = new System.Drawing.Point(340, 260);
+            this.NumDoc.Name = "NumDoc";
+            this.NumDoc.Size = new System.Drawing.Size(254, 22);
+            this.NumDoc.TabIndex = 9;
+            this.NumDoc.TextChanged += new System.EventHandler(this.NumDoc_TextChanged);
             // 
             // btGuardarRegis
             // 
@@ -148,14 +151,33 @@
             this.BtCancelar.UseVisualStyleBackColor = true;
             this.BtCancelar.Click += new System.EventHandler(this.BtCancelar_Click);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(122, 319);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Registrar Pin";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(340, 313);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(254, 22);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
             // Registrar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.BtCancelar);
             this.Controls.Add(this.btGuardarRegis);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.NumDoc);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -167,6 +189,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Registrar";
             this.Text = "Registrar";
+            this.Load += new System.EventHandler(this.Registrar_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,8 +206,10 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox NumDoc;
         private System.Windows.Forms.Button btGuardarRegis;
         private System.Windows.Forms.Button BtCancelar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
--- CajeroAslamSqlCichard/CajeroAslamSqlCichard/Registrar.cs
+++ CajeroAslamSqlCichard/CajeroAslamSqlCichard/Registrar.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace CajeroAslamSqlCichard
 {
@@ -17,16 +18,74 @@ namespace CajeroAslamSqlCichard
             InitializeComponent();
         }
 
-        private void label4_Click(object sender, EventArgs e)
-        {
 
-        }
 
         private void BtCancelar_Click(object sender, EventArgs e)
         {
             Form1 cancelar = new Form1();
             cancelar.Show();
             this.Hide();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            textBox5.MaxLength = 6;
+            string textoActual = textBox5.Text;
+            string textoFiltrado = "";
+
+            foreach (char c in textoActual)
+            {
+                // Verifica si el carácter es un dígito
+                if (char.IsDigit(c))
+                {
+                    textoFiltrado += c; // Agrega el dígito al texto filtrado
+                }
+            }
+
+            // Si hubo cambios en el texto (se eliminaron caracteres no numéricos), actualiza el TextBox
+            if (textoActual != textoFiltrado)
+            {
+                textBox5.Text = textoFiltrado;
+
+                // Coloca el cursor al final del texto para que la experiencia del usuario sea fluida
+                textBox5.SelectionStart = textBox5.Text.Length;
+            }
+        }
+
+        private void NumDoc_TextChanged(object sender, EventArgs e)
+        {
+            NumDoc.MaxLength = 6;
+            string textoActual = NumDoc.Text;
+            string textoFiltrado = "";
+
+            foreach (char c in textoActual)
+            {
+                // Verifica si el carácter es un dígito
+                if (char.IsDigit(c))
+                {
+                    textoFiltrado += c; // Agrega el dígito al texto filtrado
+                }
+            }
+
+            // Si hubo cambios en el texto (se eliminaron caracteres no numéricos), actualiza el TextBox
+            if (textoActual != textoFiltrado)
+            {
+                NumDoc.Text = textoFiltrado;
+
+                // Coloca el cursor al final del texto para que la experiencia del usuario sea fluida
+                NumDoc.SelectionStart = NumDoc.Text.Length;
+            }
+
+        }
+
+        private void Registrar_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
